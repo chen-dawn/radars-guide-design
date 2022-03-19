58b0205e3713e8bfb072ab822d0354b6d27970d8
Dawn Chen <dawnchen.xi96@gmail.com>
2022-03-19 03:11:08 -0400

    Delete frontend/node_modules directory

--- frontend/node_modules/@mui/x-data-grid-pro/hooks/features/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './columnPinning';
-export * from './columnReorder';
-export * from './columnResize';
-export * from './rowGrouping';
-export * from './treeData';
-export * from './detailPanel';
